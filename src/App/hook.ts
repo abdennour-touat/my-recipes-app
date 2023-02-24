0ac24dd737e37583e67e0b67fe8144f73a434583
abdennour touat <touatabdenour33@gmail.com>
2023-02-24 19:25:05 +0100

    displaying the data

--- src/App/hook.ts
+++ src/App/hook.ts
@@ -1,4 +1,3 @@
-import type { RootState, AppDispatch } from "./store";
 import axios from "axios";
 import { create } from "zustand";
 interface Recipe {
@@ -10,15 +9,36 @@ interface Recipe {
 
 export interface RecipeState {
   data: Recipe[];
-  getRecipe: () => void;
+  isloading: boolean;
+  query: String;
+  searchRecipe: (query: String) => void;
+  getRecipeCard: (id: number) => void;
 }
 
+const config = {
+  headers: { "x-api-key": "d7d55a1822d040c39c0e3b6af8c1b38a" },
+};
+const apikey = "d7d55a1822d040c39c0e3b6af8c1b38a";
 export const useStore = create<RecipeState>((set) => ({
+  query: "",
   data: [],
-  getRecipe: async () => {
+  isloading: true,
+  searchRecipe: async (q: String) => {
+    set(() => ({ query: q }));
     const response = await axios.get(
-      `https://api.spoonacular.com/recipes/3/information?apiKey=d7d55a1822d040c39c0e3b6af8c1b38a`
+      `https://api.spoonacular.com/recipes/complexSearch?query=${q}&apiKey=${apikey}`
+      // `https://api.spoonacular.com/recipes/3/information?apiKey=${apikey}`
+      // `https://api.spoonacular.com/search?q=${query}&apiKey=${apikey}`
     );
-    console.log(response);
+    const data = response.data.results;
+
+    set(() => ({ data, isloading: false }));
+  },
+  getRecipeCard: async (id: number) => {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/card?apiKey=${apikey}`
+      // config
+    );
+    console.log(response.data);
   },
 }));
--- src/Screens/Search.tsx
+++ src/Screens/Search.tsx
@@ -1,44 +1,32 @@
 import Card from "../components/Card";
 import NavBar from "../components/NavBar";
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Circles } from "react-loader-spinner";
+import { useStore } from "../App/hook";
+import { stat } from "fs";
 // import { useAppDispatch, useAppSelector } from "../App/hook";
 // import { getRecipes, setRecipeName } from "../features/recipe/recipeSlice";
-
 function Search() {
   const navigate = useNavigate();
   const [search, setSearch] = useState("");
   // const dispatch = useAppDispatch();
   // const { data, status } = useAppSelector((state) => state.recipes);
+  const { data, isloading, query } = useStore((state) => state);
 
   useEffect(() => {
-    // if (!data.recipeName) {
-    // navigate("/");
-    // }
+    console.log(query);
+
+    if (!query) {
+      navigate("/");
+    }
   }, []);
 
-  if (status === "rejected") {
+  if (isloading) {
     return (
-      <>
-        <NavBar
-          search={search}
-          onUpdateSearch={(e: {
-            target: { value: React.SetStateAction<string> };
-          }) => {
-            setSearch(e.target.value);
-          }}
-          onGetSearch={(e: { preventDefault: () => void }) => {
-            e.preventDefault();
-            // dispatch(setRecipeName(search));
-            // dispatch(getRecipes({ recipeName: search, recipesNumber: 5 }));
-            setSearch("");
-          }}
-        />
-        <div className="text-center mt-6">
-          <h1 className="text-xl"> An error occured</h1>
-        </div>
-      </>
+      <div className="flex justify-center pt-28">
+        <Circles color="#4ADE80" height={100} width={100} />
+      </div>
     );
   }
   return (
@@ -54,23 +42,15 @@ function Search() {
         }}
         onGetSearch={(e: { preventDefault: () => void }) => {
           e.preventDefault();
-          // dispatch(setRecipeName(search));
-          // dispatch(getRecipes({ recipeName: search, recipesNumber: 5 }));
           setSearch("");
         }}
       />
 
-      {status === "pending" ? (
-        <div className="flex justify-center pt-28">
-          <Circles color="#4ADE80" height={100} width={100} />
-        </div>
-      ) : (
-        <div className=" flex flex-wrap px-8 justify-around  font-sans mt-24 md:mt-8 ">
-          {/* {data.recipes.map((r, index) => (
-            <Card key={index} {...r}></Card> */}
-          {/* ))} */}
-        </div>
-      )}
+      <div className=" flex flex-wrap px-8 justify-around  font-sans mt-24 md:mt-8 ">
+        {data.map((r, index) => (
+          <Card key={index} {...r} />
+        ))}
+      </div>
     </>
   );
 }
